0f0e1365342fa4c8a683ff155c55bd6dba47102c
이름 <.com>
2025-12-07 20:25:36 +0900

    Fix MainListContainer import path

--- src/containers/MainListContainer.svelte
+++ src/containers/MainListContainer.svelte
@@ -1,6 +1,7 @@
 <script>
     import { banners } from '../libs/store'; 
-    import MainList from '../components/main/MainList.svelte';
+    // main -> main_files 로 폴더명을 수정했습니다.
+    import MainList from '../components/main_files/MainList.svelte';
 </script>
 
 <MainList datas={$banners} />
