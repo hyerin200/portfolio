25a44b74e8623310367d01f193b6ed7f6366a9fb
이름 <.com>
2025-12-07 20:23:11 +0900

    Fix import path in UpcomingListContainer

--- src/containers/UpcomingListContainer.svelte
+++ src/containers/UpcomingListContainer.svelte
@@ -1,6 +1,6 @@
 <script>
     import { upcomings } from '../libs/store';
-    import ListUpcoming from '../components/main/ListUpcoming.svelte';
+    import ListUpcoming from '../components/common/ListUpcoming.svelte';
 </script>
 
 <div style="margin-bottom: 50px;">
